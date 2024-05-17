6d094e1f80b7cb57b55e43006ccd2192286fd097
Rahmah Putri Azzahra <93776934+rahmaptr@users.noreply.github.com>
2024-05-17 17:11:30 +0700

    chore: route, orm, dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import router from "./router/routes";
+import "dotenv/config"
+
 const app = express();
 const PORT = 3000;
 
-app.get("/", (_, res) => {
-  res.send("Hello World!");
-});
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
